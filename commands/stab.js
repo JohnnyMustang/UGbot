3b703f72a08e89d5439fe1af212b2d9d6fbdec13
Johnny <jona.mustang@gmail.com>
2017-07-30 18:27:38 -0300

    stab 3

--- commands/stab.js
+++ commands/stab.js
@@ -1,9 +1,9 @@
 exports.run = (client, message, args) => {
     let stabee = args.slice(1).join(' ');
     let user = message.mentions.users.first();
-    if (stabee.length < 1) return message.reply('just stabbed themselves! :dagger::scream: Someone call the police!'); {
+    if (stabee.length < 1) return message.reply('just stabbed themselves! :dagger::scream: Someone call the police!');
     message.reply(`just stabbed {$user} :dagger::scream:`);
-}};
+};
 
 exports.conf = {
     enabled: true,
