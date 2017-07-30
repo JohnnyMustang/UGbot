c68288884ba3c43074530ab6a35027ff2bb545f7
Unknown <jona.mustang@gmail.com>
2017-07-30 10:14:53 -0300

    Attempt 3 ffs

--- commands/autoResponse.js
+++ commands/autoResponse.js
@@ -2,7 +2,8 @@ exports.run = (client, message) => {
 	let autoResponse = {
   "hej": "hej",
   "ayy": "lmao",
-  "ayyy": "lmao"
+  "ayyy": "lmao",
+  "ayyyy": "lmao"
 }
   
   if(autoResponse[message.content]) {
