f3e272bf4db4c7b339b0e3d94b75a44a8b072bdc
JohnnyMustang <jona.mustang@gmail.com>
2017-08-11 20:51:42 -0300

    asdsd

--- app.js
+++ app.js
@@ -12,15 +12,15 @@ const log = message => {
 
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
-fs.readdir("./commands/", (err, files) => {
+fs.readdir('./commands/', (err, files) => {
 	if (err) console.error(err);
 	log(`Loading a total of ${files.length} commands.`);
 	files.forEach(f => {
-		let props = require(`./commands/${f}`);
-		log(`Loading Command: ${props.help.name}. 👌`);
-		client.commands.set(props.help.name, props);
-		props.conf.aliases.forEach(alias => {
-			client.aliases.set(alias, props.help.name);
+	  let props = require(`./commands/${f}`);
+	  log(`Loading Command: ${props.help.name}. 👌`);
+	  client.commands.set(props.help.name, props);
+	  props.conf.aliases.forEach(alias => {
+		client.aliases.set(alias, props.help.name);
 		});
 	});
 });
